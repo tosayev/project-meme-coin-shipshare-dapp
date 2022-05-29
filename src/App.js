fc2a1ac09a37773fd1b47d1e6c9a3257f6033198
tosayev <k@tosayev.com>
2022-05-29 20:16:16 +0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,241 @@
+import { useState, useEffect } from 'react';
+import { ethers, utils } from "ethers";
+import abi from "./contracts/ShipShareCoin.json";
+
+function App() {
+  const [isWalletConnected, setIsWalletConnected] = useState(false);
+  const [inputValue, setInputValue] = useState({ walletAddress: "", transferAmount: "", burnAmount: "", mintAmount: "" });
+  const [tokenName, setTokenName] = useState("");
+  const [tokenSymbol, setTokenSymbol] = useState("");
+  const [tokenTotalSupply, setTokenTotalSupply] = useState(0);
+  const [isTokenOwner, setIsTokenOwner] = useState(false);
+  const [tokenOwnerAddress, setTokenOwnerAddress] = useState(null);
+  const [yourWalletAddress, setYourWalletAddress] = useState(null);
+  const [error, setError] = useState(null);
+
+  const contractAddress = '0xDd793F17DE38383c4eEF74DA4288e2Fdc2AA843D';
+  const contractABI = abi.abi;
+
+  const checkIfWalletIsConnected = async () => {
+    try {
+      if (window.ethereum) {
+        const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+        const account = accounts[0];
+        setIsWalletConnected(true);
+        setYourWalletAddress(account);
+        console.log("Account connected: ", account);
+      } else {
+        setError("Install a MetaMask wallet to get our token.");
+        console.log("No Metamask detected");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const getTokenInfo = async () => {
+    try {
+      if (window.ethereum) {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        const tokenContract = new ethers.Contract(contractAddress, contractABI, signer);
+        const [account] = await window.ethereum.request({ method: 'eth_requestAccounts' });
+
+        let tokenName = await tokenContract.name();
+        let tokenSymbol = await tokenContract.symbol();
+        let tokenOwner = await tokenContract.owner();
+        let tokenSupply = await tokenContract.totalSupply();
+        tokenSupply = utils.formatEther(tokenSupply);
+
+        setTokenName(`${tokenName} 🚢`);
+        setTokenSymbol(tokenSymbol);
+        setTokenTotalSupply(tokenSupply);
+        setTokenOwnerAddress(tokenOwner);
+
+        if (account.toLowerCase() === tokenOwner.toLowerCase()) {
+          setIsTokenOwner(true);
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const transferToken = async (event) => {
+    event.preventDefault();
+    try {
+      if (window.ethereum) {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        const tokenContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+        const txn = await tokenContract.transfer(inputValue.walletAddress, utils.parseEther(inputValue.transferAmount));
+        console.log("Transfering", inputValue.transferAmount, tokenSymbol, "to address:", inputValue.walletAddress, "...");
+        await txn.wait();
+        console.log(inputValue.transferAmount, tokenSymbol, "transferred to", inputValue.walletAddress, "; TXN:", txn.hash);
+
+      } else {
+        console.log("Ethereum object not found, install Metamask.");
+        setError("Install a MetaMask wallet to get our token.");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const burnTokens = async (event) => {
+    event.preventDefault();
+    try {
+      if (window.ethereum) {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        const tokenContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+        const txn = await tokenContract.burn(utils.parseEther(inputValue.burnAmount));
+        console.log("Burning", inputValue.burnAmount, tokenSymbol, "...");
+        await txn.wait();
+        console.log(inputValue.burnAmount, tokenSymbol, "burned ; TXN:", txn.hash);
+
+        let tokenSupply = await tokenContract.totalSupply();
+        tokenSupply = utils.formatEther(tokenSupply);
+        setTokenTotalSupply(tokenSupply);
+
+      } else {
+        console.log("Ethereum object not found, install Metamask.");
+        setError("Install a MetaMask wallet to get our token.");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const mintTokens = async (event) => {
+    event.preventDefault();
+    try {
+      if (window.ethereum) {
+        const provider = new ethers.providers.Web3Provider(window.ethereum);
+        const signer = provider.getSigner();
+        const tokenContract = new ethers.Contract(contractAddress, contractABI, signer);
+
+        let tokenOwner = await tokenContract.owner();
+        const txn = await tokenContract.mint(tokenOwner, utils.parseEther(inputValue.mintAmount));
+        console.log("Minting", inputValue.mintAmount, tokenSymbol, "...");
+        await txn.wait();
+        console.log(inputValue.mintAmount, tokenSymbol, "minted ; TXN:", txn.hash);
+
+        let tokenSupply = await tokenContract.totalSupply();
+        tokenSupply = utils.formatEther(tokenSupply);
+        setTokenTotalSupply(tokenSupply);
+      } else {
+        console.log("Ethereum object not found, install Metamask.");
+        setError("Install a MetaMask wallet to get our token.");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  const handleInputChange = (event) => {
+    setInputValue(prevFormData => ({ ...prevFormData, [event.target.name]: event.target.value }));
+  }
+
+  useEffect(() => {
+    checkIfWalletIsConnected();
+    getTokenInfo();
+  }, [])
+
+  return (
+    <main className="main-container">
+      <h2 className="headline">
+        <span className="headline-gradient">Ship Share Coin Project</span>
+        <img className="inline p-3 ml-2" src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/twitter/322/ship_1f6a2.png" alt="Meme Coin" width="60" height="30" />
+      </h2>
+      <section className="customer-section px-10 pt-5 pb-10">
+        {error && <p className="text-2xl text-red-700">{error}</p>}
+        <div className="mt-5">
+          <p>ShipShareCoin is a token that represents 1/40 millionth share of a 40 million USD crude oil tanker.</p>
+        </div>
+        <div className="mt-5">
+          <span className="mr-5"><strong>Coin:</strong> {tokenName} </span>
+          <span className="mr-5"><strong>Ticker:</strong>  {tokenSymbol} </span>
+          <span className="mr-5"><strong>Total Supply:</strong>  {Math.round(tokenTotalSupply).toLocaleString('en-US')}</span>
+        </div>
+        <div className="mt-7 mb-9">
+          <form className="form-style">
+            <input
+              type="text"
+              className="input-double"
+              onChange={handleInputChange}
+              name="walletAddress"
+              placeholder="Wallet Address"
+              value={inputValue.walletAddress}
+            />
+            <input
+              type="text"
+              className="input-double"
+              onChange={handleInputChange}
+              name="transferAmount"
+              placeholder={`0.0000 ${tokenSymbol}`}
+              value={inputValue.transferAmount}
+            />
+            <button
+              className="btn-purple"
+              onClick={transferToken}>Transfer Tokens</button>
+          </form>
+        </div>
+        {isTokenOwner && (
+          <section>
+            <div className="mt-10 mb-10">
+              <form className="form-style">
+                <input
+                  type="text"
+                  className="input-style"
+                  onChange={handleInputChange}
+                  name="burnAmount"
+                  placeholder={`0.0000 ${tokenSymbol}`}
+                  value={inputValue.burnAmount}
+                />
+                <button
+                  className="btn-purple"
+                  onClick={burnTokens}>
+                  Burn Tokens
+                </button>
+              </form>
+            </div>
+            <div className="mt-10 mb-10">
+              <form className="form-style">
+                <input
+                  type="text"
+                  className="input-style"
+                  onChange={handleInputChange}
+                  name="mintAmount"
+                  placeholder={`0.0000 ${tokenSymbol}`}
+                  value={inputValue.mintAmount}
+                />
+                <button
+                  className="btn-purple"
+                  onClick={mintTokens}>
+                  Mint Tokens
+                </button>
+              </form>
+            </div>
+          </section>
+        )}
+        <div className="mt-5">
+          <p><span className="font-bold">Contract Address: </span>{contractAddress}</p>
+        </div>
+        <div className="mt-5">
+          <p><span className="font-bold">Token Owner Address: </span>{tokenOwnerAddress}</p>
+        </div>
+        <div className="mt-5">
+          {isWalletConnected && <p><span className="font-bold">Your Wallet Address: </span>{yourWalletAddress}</p>}
+          <button className="btn-connect" onClick={checkIfWalletIsConnected}>
+            {isWalletConnected ? "Wallet Connected 🔒" : "Connect Wallet 🔑"}
+          </button>
+        </div>
+
+      </section>
+    </main>
+  );
+}
+export default App;
